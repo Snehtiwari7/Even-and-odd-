8a2f9f857a6b2f1251f2269a88e7cf056eeeb6fa
Snehtiwari7 <tiwarisneh688@gmail.com>
2026-08-07 12:45:55 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter your no.";
+    cin>>num;
+    if(num%2==0)
+    cout<<"No. is even";
+    else
+    cout<<"No. is odd";
+    
+}
